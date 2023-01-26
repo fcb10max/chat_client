040376a689226f99a91c70aac2ff40de434875bf
Max <fcb10max@gmail.com>
2023-01-26 14:04:58 +0500

    auth basics completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import {
   BrowserRouter,
   Navigate,
@@ -6,12 +6,7 @@ import {
   Routes,
   useLocation,
 } from "react-router-dom";
-import { io, Socket } from "socket.io-client";
 import "./globalStyles.module.scss";
-import {
-  ClientToServerEvents,
-  ServerToClientEvents,
-} from "./interfaces/socket";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import Register from "./pages/Register";
@@ -30,49 +25,14 @@ const Wrapper: React.FC<IWrapperProps> = ({ children }) => {
   return children;
 };
 
-interface IUser {
-  id: number;
-}
 
 const App = () => {
-  const [user, setUser] = useState<IUser | null>(null);
-  const [socket, setSocket] = useState<Socket | null>(null);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:3000/api/auth/checkToken", {
-  //     credentials: "include",
-  //   })
-  //     .then((d) => d.json())
-  //     .then((d) => console.log(d));
-  // }, []);
-
-  useEffect(() => {
-    if (!user) return;
-    const newSocket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-      process.env.REACT_APP_WS_SERVER_URL ?? "ws://localhost:3000/"
-    );
-    setSocket(newSocket);
-    return () => {
-      newSocket.disconnect();
-    };
-  }, [user]);
-
   return (
     <BrowserRouter>
       <main>
         <Wrapper>
           <Routes>
-            <Route
-              path="/"
-              element={
-                <NotFound />
-                // <Navigate
-                //   to={
-                //     user && user.id ? `/user/dashboard` : "/login"
-                //   }
-                // />
-              }
-            />
+            <Route path="/" element={<Navigate to={`/user/dashboard`} />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/user/dashboard" element={<UserDashboard />} />
--- src/pages/UserDashoard/index.tsx
+++ src/pages/UserDashoard/index.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { io, Socket } from "socket.io-client";
+import {
+  ClientToServerEvents,
+  ServerToClientEvents,
+} from "../../interfaces/socket";
 
-const UserDashboard = () => {
-  return (
-    <div>UserDashboard</div>
-  )
+interface IUser {
+  id: number;
 }
 
-export default UserDashboard
+const UserDashboard = () => {
+  const [user, setUser] = useState<IUser | null>(null);
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isError, setIsError] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/auth/checkToken", {
+      credentials: "include",
+    })
+      .then((d) => d.json())
+      .then((d) => {
+        if (!d.success) {
+          if (d.errMsg === "Token not found") {
+            return navigate("/login")
+          }
+          setIsError(true);
+          setErrorMessage(d.errMsg);
+          return;
+        }
+        setUser(d.user);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    if (!user) return;
+    const newSocket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+      process.env.REACT_APP_WS_SERVER_URL ?? "ws://localhost:3000/"
+    );
+    setSocket(newSocket);
+    return () => {
+      newSocket.disconnect();
+    };
+  }, [user]);
+
+  return <div>{isError && <p>{errorMessage}</p>}</div>;
+};
+
+export default UserDashboard;
